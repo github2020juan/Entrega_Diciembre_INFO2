f113ac20dc856f550f7ea581fdec867178f57e0a
github2020juan <95059076+github2020juan@users.noreply.github.com>
2021-12-11 22:27:51 -0300

    Add files via upload

---
+++ data/Aeropuertos_5.js
@@ -0,0 +1,9 @@
+var json_Aeropuertos_5 = {
+"type": "FeatureCollection",
+"name": "Aeropuertos_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Aeropuerto Internacional Pudong (PVG)" }, "geometry": { "type": "Point", "coordinates": [ 109.327935071371755, 16.415728309614199 ] } },
+{ "type": "Feature", "properties": { "name": "Aeropuerto Internacional Hongqiao (SHA)" }, "geometry": { "type": "Point", "coordinates": [ 109.327930611127101, 16.415728537589054 ] } }
+]
+}
---
+++ data/puertosha_6.js
@@ -0,0 +1,8 @@
+var json_puertosha_6 = {
+"type": "FeatureCollection",
+"name": "puertosha_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "name": "Puerto Internacional de Shangai" }, "geometry": { "type": "Point", "coordinates": [ 109.327933625918078, 16.415729853166258 ] } }
+]
+}
